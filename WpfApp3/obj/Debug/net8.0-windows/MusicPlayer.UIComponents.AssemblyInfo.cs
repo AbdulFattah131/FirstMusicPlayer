5b5b752bd6c8cce68cc2ccd33c5e929043697562
Syed Abdul Fattah Alam <fattahalam131@gmail.com>
2025-11-11 14:26:19 +0500

    #00053 (Update 11/11 2 PM)

--- WpfApp3/obj/Debug/net8.0-windows/MusicPlayer.UIComponents.AssemblyInfo.cs
+++ WpfApp3/obj/Debug/net8.0-windows/MusicPlayer.UIComponents.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicPlayer.UIComponents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f5128c47b758ff4171e8a3016aa69a87a3220b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c5e669efbcb9d06cadf5341bed80515faf1b48")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicPlayer.UIComponents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicPlayer.UIComponents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
